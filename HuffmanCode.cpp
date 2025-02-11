929f229a9584732b93efb7ffa7a3c0e6f0729fcf
RevanthAgulla <sairevanthkrishna.agulla@cuw.edu>
2025-02-11 13:42:11 -0600

    HuffmanCode

---
+++ HuffmanCode.cpp
@@ -0,0 +1,92 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+#include <map>
+#include <string>
+
+using namespace std;
+
+// Huffman Tree node
+struct Node {
+    char data;
+    int freq;
+    Node *left, *right;
+
+    Node(char data, int freq) {
+        this->data = data;
+        this->freq = freq;
+        left = right = nullptr;
+    }
+};
+
+// Function for comparing min heap
+struct Compare {
+    bool operator()(Node* l, Node* r) {
+        return (l->freq > r->freq);
+    }
+};
+
+// The Huffman tree-building function
+Node* buildHuffmanTree(string s, int f[]) {
+    priority_queue<Node*, vector<Node*>, Compare> minHeap;
+
+    for (int i = 0; i < s.length(); ++i) {
+        minHeap.push(new Node(s[i], f[i]));
+    }
+
+    while (minHeap.size() > 1) {
+        Node* left = minHeap.top();
+        minHeap.pop();
+
+        Node* right = minHeap.top();
+        minHeap.pop();
+
+        Node* internalNode = new Node('\0', left->freq + right->freq); // '\0' for internal nodes
+        internalNode->left = left;
+        internalNode->right = right;
+        minHeap.push(internalNode);
+    }
+
+    return minHeap.top();
+}
+
+// Huffman code printing function (preorder traversal)
+void printCodes(Node* root, string code, map<char, string>& huffmanCodes) {
+    if (!root) return;
+
+    if (root->data != '\0') {  // Leaf node
+        huffmanCodes[root->data] = code;
+        cout << code << " "; // Print the code
+    }
+
+    printCodes(root->left, code + "0", huffmanCodes);
+    printCodes(root->right, code + "1", huffmanCodes);
+}
+
+
+int main() {
+    string s;
+    cout << "Enter the string of characters: ";
+    cin >> s;
+
+    int f[s.length()];
+    cout << "Enter the frequencies for each character: ";
+    for (int i = 0; i < s.length(); ++i) {
+        cin >> f[i];
+    }
+
+
+    Node* root = buildHuffmanTree(s, f);
+    map<char, string> huffmanCodes;
+    printCodes(root, "", huffmanCodes);
+    cout << endl;
+
+    // Printing the codes in a formatted manner is optional.
+    /*
+    for (auto const& [key, val] : huffmanCodes){
+        cout << key << " : " << val << endl;
+    }
+    */
+
+    return 0;
+}
